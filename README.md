926e40b3ade828fd2e9cedbb26a550e7feccd860
rhinod <isc.damians@gmail.com>
2020-06-04 00:12:01 -0700

    Add font-awesome gem, Add search route, fixed iex api sandbox, Create user controller

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is the finance tracker app from the Complete Ruby on Rails Developer course
 
 * Ruby version. 6.0.3
 rails s -b 0.0.0.0 -p 80
+"C:\Program Files\Notepad++\notepad++.exe"
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,7 +3,9 @@ class Stock < ApplicationRecord
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_token],
                                       secret_token: Rails.application.credentials.iex_client[:secret_token],
-                                      endpoint: 'https://cloud.iexapis.com/v1')
-        client.price(ticker_symbol)
+                                      endpoint: 'https://sandbox.iexapis.com/v1')
+        new(ticker: ticker_symbol ,name: client.company(ticker_symbol).company_name  ,last_price: client.price(ticker_symbol))
+
+
     end
 end
