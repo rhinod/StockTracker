e7d830de8abd40ccb32f03696fc39b2937c4b159
rhinod <isc.damians@gmail.com>
2020-06-10 03:55:05 -0700

    Add friend function in front-end

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,14 @@
 class FriendshipsController < ApplicationController
     def create
+      friend = User.find(params[:friend])
+      current_user.friendships.build(friend_id: friend.id)
+
+      if current_user.save
+        flash[:notice] = "You are friends now"
+      else 
+        flash[:alert] = "Something went wrong, please try again later"
+      end
+      redirect_to my_friends_path
     end
 
     def destroy
